c305249f29f0b19730f92d19c0f13ad0a4cde668
Himanshu Maurya <mauryahimanshu979@gmail.com>
2025-04-20 15:11:42 +0530

    prisma postgreSql and Docker in action

--- Leetlab_Backend/src/index.js
+++ Leetlab_Backend/src/index.js
@@ -1,13 +1,17 @@
 import express from "express"
 import dotenv from "dotenv";
+import authRoute from "./Routes/auth.routes.js";
 
 dotenv.config();
 const app = express();
 
+app.use(express.json());
 
 app.use("/",(req,res)=>{
-    res.send("Welcome to Leetlab");
+    res.send("Welcome to Leetlab🔥");
 })
+
+app.use("/api/v1/auth",authRoute)
 app.listen(process.env.PORT,()=>{
     console.log("Server is running on port 8080")
 })
