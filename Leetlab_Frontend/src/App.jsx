ee6758c4ba771ef61fa6b6c989ea41479bed6fdc
Himanshu Maurya <mauryahimanshu979@gmail.com>
2025-05-10 20:47:29 +0530

    Project setup

---
+++ Leetlab_Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Routes,Route, Navigate} from "react-router-dom"
+
+
+const App = () => {
+  return (
+    <div className='flex flex-col items-center justify-start'>
+      <h1>Hello ji </h1>
+    </div>
+  )
+}
+
+export default App
