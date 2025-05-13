5483eb59527f4c0da2b986a816ef43f7375ef2e8
Himanshu Maurya <mauryahimanshu979@gmail.com>
2025-05-13 18:13:00 +0530

    Problem Table Component added on Home Page

--- Leetlab_Frontend/src/page/HomePage.jsx
+++ Leetlab_Frontend/src/page/HomePage.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useProblemStore } from "../store/useProblemStore";
+import { Loader } from "lucide-react";
+import ProblemTable from "../components/ProblemTable";
 
 const HomePage = () => {
+  const { getAllProblems, problems, isProblemLoading } = useProblemStore();
+
+  useEffect(() => {
+    getAllProblems();
+  }, [getAllProblems]);
+
+  console.log(problems);
+
+  if (isProblemLoading) {
+    return (
+      <div className="flex item-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
   return (
     <>
       <div className="min-h-screen flex flex-col items-center mt-14 px-4">
@@ -14,6 +32,14 @@ const HomePage = () => {
           interviews and helps you to improve your coding skills by solving
           coding problems
         </p>
+
+        {problems.length > 0 ? (
+          <ProblemTable problems={problems} />
+        ) : (
+          <p className="mt-10 text-center text-lg font-semibold text-gray-500 dark:text-gray-400 z-10 border border-primary px-4 py-2 rounded-md border-dashed">
+            No problems found
+          </p>
+        )}
       </div>
     </>
   );
